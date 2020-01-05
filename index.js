74eb3bddf6afd0b7552999836ebfcc8f48471804
chjin <seedevk8s@gmail.com>
2020-01-05 16:32:14 +0900

    js 연결 및 속성 확인

---
+++ index.js
@@ -0,0 +1,7 @@
+const number = document.getElementById('number');
+const increase = document.getElementById('increase');
+const decrease = document.getElementById('decrease');
+
+console.log(number.innerText);
+console.log(increase.offsetTop);
+console.log(decrease.id);
