ba5e2fb885d2d27ec6298954eb087d92d17c0ef0
chjin <seedevk8s@gmail.com>
2020-01-05 16:38:19 +0900

    onclick 이벤트 추가함--두버튼에 증가,감소 기능 추가함

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const increase = document.getElementById('increase');
 const decrease = document.getElementById('decrease');
 
 increase.onclick = () => {
-    console.log('increase 가 클릭됨');
+    const current = parseInt(number.innerText, 10);
+    number.innerText = current + 1;
 }
 
 decrease.onclick = () => {
-    console.log('decrease 가 클릭됨');
+    const current = parseInt(number.innerText, 10);
+    number.innerText = current - 1;
 }
 
