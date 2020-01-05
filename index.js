feb93dff8348aef0e2586d888c2e8144e6528a79
chjin <seedevk8s@gmail.com>
2020-01-05 16:58:21 +0900

    모달 만들기--원소스

--- index.js
+++ index.js
@@ -1,19 +1,4 @@
-const number = document.getElementById('number');
-const buttons = document.querySelectorAll('button');
-const [increase, decrease] = buttons;
-
-
-increase.onclick = () => {
-    const current = parseInt(number.innerText, 10);    //number에 있는 문자를 꺼내와서 10진수로 파싱해 저장
-    number.innerText = current + 1;
-}
-
-decrease.onclick = () => {
-    const current = parseInt(number.innerText, 10);
-    number.innerText = current - 1;
-}
-
-
+import "./styles.css";
 
 
 
