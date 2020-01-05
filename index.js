b5520ac2d13ab5f7cc44c678e5baf30f4d4d4016
chjin <seedevk8s@gmail.com>
2020-01-05 17:36:03 +0900

    이벤트 구현

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 //import "./styles.css";
 
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+const modal = document.querySelector('.modal-wrapper');
+
+open.onclick = () => {
+    modal.style.display = 'flex';
+}
+
+close.onclick = () => {
+    modal.style.display = 'none';
+}
 
 
 
